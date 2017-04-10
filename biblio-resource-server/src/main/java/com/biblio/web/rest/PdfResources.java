a78d56d8be1028c31b7a7b4af14ff4cdf902a887
kouwonou <kouwonou@kouwonou-HP-Pavilion-15-Notebook-PC>
2017-04-10 21:00:38 +0000

    Merge origin/developpers into developpers
    Conflicts:
    	module-heritage/src/main/java/com/ncp/assurance/entities/heritage/DAO/ICommissionNCPCourtierPointsVenteTypeAssuranceDAO.java
    	module-web/src/main/webapp/WEB-INF/jdbc.properties

---
+++ biblio-resource-server/src/main/java/com/biblio/web/rest/PdfResources.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.biblio.web.rest;
+
+import com.biblio.entity.Livre;
+import com.biblio.repository.LivreRepository;
+import com.biblio.web.rest.util.GenerateQRCode;
+import com.google.zxing.WriterException;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.List;
+import com.itextpdf.text.ListItem;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.AcroFields;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.FdfWriter;
+import com.itextpdf.text.pdf.PdfAcroForm;
+import com.itextpdf.text.pdf.PdfFormField;
+import com.itextpdf.text.pdf.PdfName;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.TextField;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import javax.imageio.ImageIO;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author kouwonou
+ */
+@RestController
+@RequestMapping(value = "/api/pdf")
+public class PdfResources {
+
+    @Inject
+    private LivreRepository livreRepository;
+
+    @RequestMapping(value = "livre", method = RequestMethod.GET)
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, WriterException {
+        response.setContentType("application/pdf");
+        GenerateQRCode ge = new GenerateQRCode();
+
+        try {
+
+            Document document = new Document();
+            String param = request.getParameter("isbn");
+            Livre livre = livreRepository.findOneByIsbn(param).get();
+            PdfWriter e = PdfWriter.getInstance(document, response.getOutputStream());
+
+            document.open();
+
+
+             Font font = new Font();
+             
+             font.setStyle(Font.BOLD);
+             font.setSize(12);
+              
+            List list = new List(15);
+
+
+            
+            //  document.left(12);
+            list.add(new ListItem("Titre  :" + livre.getTitre(), font));
+            list.add(new ListItem("Categorie  :" + livre.getCategorie().getDescription(), font));
+            list.add(new ListItem("Auteurs   :" + livre.getAuteurs(), font));
+            list.add(new ListItem("Edition   :" + livre.getEdition(), font));
+            list.add(new ListItem("Editeur   :" + livre.getEditeur(), font));
+            list.add(new ListItem("Collection   :" + livre.getCollection(), font));
+            list.add(new ListItem("Date parution   :" + livre.getDateParution(), font));
+            list.add(new ListItem("Isbn   " + livre.getIsbn(), font));
+            list.add(new ListItem("Resume   : " + livre.getResume(), font));
+
+            document.add(list);
+            document.addTitle(livre.getTitre());
+            document.setMargins(100, 20, 0, 0);
+            document.addCreationDate();
+            System.out.println("TTT v "+  document.addTitle(param));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(ge.createQRImage(param, 125), "jpg", baos);
+            Image image = Image.getInstance(baos.toByteArray());
+            document.add(image);
+            document.close();
+
+        } catch (DocumentException de) {
+            throw new IOException(de.getMessage());
+        }
+    }
+}
---
+++ biblio-resource-server/src/main/java/com/biblio/web/rest/util/GenerateQRCode.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.biblio.web.rest.util;
+
+/**
+ *
+ * @author kouwonou
+ */
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GenerateQRCode {
+
+    /**
+     * @param qrCodeText
+     * @param size
+     * @param fileType
+     * @return 
+     * @throws WriterException
+     * @throws IOException
+     */
+//    public static void main(String[] args) throws WriterException, IOException {
+//        String qrCodeText = "http://www.journaldev.com";
+//        String filePath = System.getProperty("user.home")+"/qrcode/J1D.png";
+//        int size = 125;
+//        String fileType = "png";
+//        //File qrFile = new File(filePath);
+//        createQRImage(qrFile, qrCodeText, size, fileType);
+//      
+//    }
+
+   public BufferedImage createQRImage(String qrCodeText, int size) throws WriterException, IOException {
+        // Create the ByteMatrix for the QR-Code that encodes the given String
+        Map hintMap = new HashMap();
+        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+       
+        BitMatrix byteMatrix = qrCodeWriter.encode(qrCodeText,
+                BarcodeFormat.QR_CODE, size, size, hintMap);
+        // Make the BufferedImage that are to hold the QRCode
+        int matrixWidth = byteMatrix.getWidth();
+        BufferedImage image = new BufferedImage(matrixWidth, matrixWidth,
+                BufferedImage.TYPE_INT_RGB);
+        image.createGraphics();
+
+        Graphics2D graphics = (Graphics2D) image.getGraphics();
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, matrixWidth, matrixWidth);
+        // Paint and save the image using the ByteMatrix
+        graphics.setColor(Color.BLACK);
+
+        for (int i = 0; i < matrixWidth; i++) {
+            for (int j = 0; j < matrixWidth; j++) {
+                if (byteMatrix.get(i, j)) {
+                    graphics.fillRect(i, j, 1, 1);
+                }
+            }
+        }
+       return image;
+    }
+
+}
