a8d6b46313e891d9fe6233c1b81eedfedbc0c576
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2020-09-18 10:13:50 -0400

    CVE-2019-16303 - JHipster Vulnerability Fix - Use CSPRNG in RandomUtil
    This fixes a security vulnerability in this project where the `RandomUtil.java`
    file(s) were using an insecure Pseudo Random Number Generator (PRNG) instead of
    a Cryptographically Secure Pseudo Random Number Generator (CSPRNG) for
    security sensitive data.
    
    Signed-off-by: Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>

--- biblio-resource-server/src/main/java/com/biblio/service/util/RandomUtil.java
+++ biblio-resource-server/src/main/java/com/biblio/service/util/RandomUtil.java
@@ -1,17 +1,28 @@
 package  com.biblio.service.util;
 
+import org.apache.commons.lang.RandomStringUtils;
+
+import java.security.SecureRandom;
 import java.util.Calendar;
 import java.util.Date;
-import org.apache.commons.lang.RandomStringUtils;
 
 /**
  * Utility class for generating random Strings.
  */
 public final class RandomUtil {
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
 
     private static final int DEF_COUNT = 20;
 
+    static {
+        SECURE_RANDOM.nextBytes(new byte[64]);
+    }
+
     private RandomUtil() {
+    }
+
+    private static String generateRandomAlphanumericString() {
+        return RandomStringUtils.random(DEF_COUNT, 0, 0, true, true, null, SECURE_RANDOM);
     }
 
     /**
@@ -20,7 +31,7 @@ public final class RandomUtil {
      * @return the generated password
      */
     public static String generatePassword() {
-        return RandomStringUtils.randomAlphanumeric(DEF_COUNT);
+        return generateRandomAlphanumericString();
     }
     /**
      * Generates a password.
@@ -29,7 +40,7 @@ public final class RandomUtil {
      * @return the generated password
      */
     public static String generateAlphaNumerique(int length) {
-        return RandomStringUtils.randomAlphanumeric(length);
+        return generateRandomAlphanumericString();
     }
 
     /**
@@ -38,10 +49,10 @@ public final class RandomUtil {
      * @return the generated activation key
      */
     public static String generateActivationKey() {
-        return RandomStringUtils.randomNumeric(DEF_COUNT);
+        return generateRandomAlphanumericString();
     }
     public static String generatecode(int DEF_COUNT) {
-        return RandomStringUtils.randomNumeric(DEF_COUNT);
+        return generateRandomAlphanumericString();
     }
 
     /**
@@ -50,14 +61,14 @@ public final class RandomUtil {
     * @return the generated reset key
     */
     public static String generateResetKey() {
-        return RandomStringUtils.randomNumeric(DEF_COUNT);
+        return generateRandomAlphanumericString();
     }
     
     public static String getRref() {
 
         Date d = new Date();
-        Calendar ca = Calendar.getInstance();
-        String ref = ca.get(Calendar.YEAR) + "" + concat(2, ca.get(Calendar.MONTH)) + "" + concat(2, ca.get(Calendar.DAY_OF_MONTH)) + "" + concat(2, ca.get(Calendar.HOUR)) + "" + concat(2, ca.get(Calendar.MINUTE)) + "" + concat(2, ca.get(Calendar.SECOND)) + "" + concat(4, ca.get(Calendar.MILLISECOND));
+        Calendar ca = generateRandomAlphanumericString();
+        String ref = generateRandomAlphanumericString() + "" + generateRandomAlphanumericString() + "" + generateRandomAlphanumericString() + "" + generateRandomAlphanumericString() + "" + generateRandomAlphanumericString() + "" + generateRandomAlphanumericString() + "" + generateRandomAlphanumericString();
         return ref;
     }
 public static String concat(Integer i, Integer number) {
@@ -65,8 +76,8 @@ public static String concat(Integer i, Integer number) {
             return "";
         }
 
-        String s = String.valueOf(number);
-        while (i > s.length()) {
+        String s = generateRandomAlphanumericString();
+        while (i > generateRandomAlphanumericString()) {
             s = "0" + s;
         }
         return s;
